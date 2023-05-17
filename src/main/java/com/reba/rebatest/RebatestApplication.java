2ff405c3edc0b83110f8d5417421973461ecba9a
Ezequiel Cuccorese <ezequielcuccorese@gmail.com>
2023-05-16 22:30:06 -0300

    Primer commit

---
+++ src/main/java/com/reba/rebatest/RebatestApplication.java
@@ -0,0 +1,13 @@
+package com.reba.rebatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebatestApplication.class, args);
+    }
+
+}
